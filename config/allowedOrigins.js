d2e47a0a9a9e5133003bb69f5664f0daa329ca38
Laura Funderburk <l_funderburk@mail.fhsu.edu>
2025-05-06 14:03:06 -0500

    added testing URL

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://lfunderburk-final.glitch.me/',
     'https://inf653-final-9wps.onrender.com/',
+    'https://dazzling-snickerdoodle-777101.netlify.app/',
     'http://127.0.0.1:3500',
     'http://localhost:3500'
 ];
