8498a9756f0f0316b3edbf8ea50711835f637e08
Laura Funderburk <l_funderburk@mail.fhsu.edu>
2025-05-07 14:46:19 -0500

    updated error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,14 @@
 const { logEvents } = require('./logEvents');
 
 const errorHandler = (err, req, res, next) => {
-    logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
-    console.error(err.stack)
-    res.status(500).send(err.message);
-}
+  logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
+  console.error(err.stack);
+
+  const statusCode = err.status || 500;
+
+  res.status(statusCode).json({
+    message: err.message || 'Internal Server Error'
+  });
+};
 
 module.exports = errorHandler;
